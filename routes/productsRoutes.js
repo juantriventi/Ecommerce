3da6656a2eff4462f5fdd74ea811de0a9c3d66c1
sergiomrp <sergioporco@gmail.com>
2021-09-07 18:31:55 -0300

    actualizacion de rutas

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,32 +1,53 @@
 const { Router } = require("express");
-const express = require("express")
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const { body } = require("express-validator");
+
+/*** devielve los errores de los formularios ***/
+
+const validateProduct = [
+  body("nombre").notEmpty().withMessage("Debe Completar Este campo"),
+  //body('price').isInt({min: 1}).withMessage('El campo precio tiene que ser un numero mayor a 1'),
+  //body('discount').isInt({min: 0, max:100}).withMessage('Descuento fuera de rango'),
+  //body('category').notEmpty().withMessage('Debe Completar Este campo'),
+  //body('description').notEmpty().withMessage('Debe Completar Este campo').bail().isLength({min:0,max:200}).withMessage('Tiene que escribir menos de 200 carateres'),
+];
+
+/***  multer se encarga de almacenar archivos en el servidor express ***/
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "./public/images/products");
+  },
+  filename: (req, file, callback) => {
+    const fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
+    callback(null, fileName);
+  },
+});
+
+const uploadFile = multer({
+  storage: storage,
+});
+
 let router = express.Router();
-const productsController = require("../controllers/productsController")
 
-router.get("/carrito", productsController.carrito)
+const productosController = require("../controllers/productosController");
 
-router.get("/detalle", productsController.detalle)
+router.get("/", productosController.index);
 
-router.get("/create", productsController.create)
+router.get("/carrito", productosController.carrito);
 
-router.get("/products", )
+router.get("/detalle/:id", productosController.detalle);
 
+router.get("/create", productosController.create); // miestra la vista para crear
 
-/*
-1. /products (GET)
-Listado de productos
-2. /products/create (GET)
-Formulario de creación de productos
-3. /products/:id (GET)
-Detalle de un producto particular
-4. /products (POST)
-Acción de creación (a donde se envía el formulario)
-5. /products/:id/edit (GET)
-Formulario de edición de productos
-6. /products/:id (PUT)
-Acción de edición (a donde se envía el formulario):
-7. /products/:id (DELETE)
-Acción de borrado
-*/
+router.post("/create", uploadFile.single("imagen"), productosController.store); // procesa y almacena el producto
 
-module.exports = router 
+router.get('/create/:id', productosController.update);
+
+router.put('/create/:id', uploadFile.single("imagen"), productosController.put);
+
+router.delete("/delete/:id", productosController.remove);
+
+module.exports = router;
