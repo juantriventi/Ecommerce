d1afc8e5962a8f3bfbab59e0941e81ef8a2c7437
sergiomrp <sergioporco@gmail.com>
2021-09-07 18:36:24 -0300

    moficiacion ejs

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express=require("express");
-const app= express();
-const port=3030;
-const path=require('path');
 const mainRoutes = require("./routes/mainRoutes.js");
 const productsRoutes = require("./routes/productsRoutes.js");
 const usersRoutes = require("./routes/usersRoutes.js");
+const createErrors = require('http-errors');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const methodOverride =  require('method-override');
+const session = require('express-session');
+
+
+const app= express();
+const port=3030;
+const path=require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
-
-app.use(express.static("public"));
-
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use("/", mainRoutes);
-
 app.use("/products", productsRoutes);
-
 app.use("/users", usersRoutes);
-
 app.listen(port, () => 
-console.log("Levantando un servidor con Express en el puerto"+port)
+console.log("Levantando un servidor con Express en el puerto" + port)
 )
