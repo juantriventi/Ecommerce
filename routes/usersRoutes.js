5f44f49dc57b1b1b6eeb789b12001690fea40491
sergiomrp <sergioporco@gmail.com>
2021-10-04 02:11:23 -0300

    -

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -10,7 +10,9 @@ const { body } = require("express-validator");
 const validations = [
     body("nombre").notEmpty().withMessage("Coloca tu nombre"),
     body("apellido").notEmpty().withMessage("Coloca tu apellido"),
-    body("email").notEmpty().withMessage("Tenes que poner un mail"),
+    body("email")
+        .notEmpty().withMessage("Tenes que poner un mail").bail()
+        .isEmail().withMessage("Tienes que poner un formato de correo electronico valido"),
     body("password").notEmpty().withMessage("Pon una contraseña"),
     body("confirmPassword").notEmpty().withMessage("Repite la contraseña anterior")
 ]
