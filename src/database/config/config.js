a60fc2ab79f8b965f49eb6e79ce7ce6fa5688037
juantriventi <86171363+juantriventi@users.noreply.github.com>
2021-10-25 10:28:55 -0300

    actualizando la configuración

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,39 +1,18 @@
-// {
-//   "development": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_development",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   }
-// }
+const {INTEGER} = require("sequelize");
+
 require('dotenv').config()
 
 module.exports =
 
 {
-    "development": {
+     "development": {
 
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": process.env.DB_DIALECT,
+    "port": parseInt(process.env.DB_PORT),
+    "dialect":"mysql",
 
     // seederStorage: "sequelize",
     // seederStorageTableName: "seeds",
