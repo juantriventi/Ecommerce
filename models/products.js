7d9c28e0795ee4ff8cae89617faf99f74a537e5c
juantriventi <juantriventi@hotmail.com>
2021-11-02 00:13:36 -0300

    creacion de funcion search y modificacion de const producto

--- models/products.js
+++ models/products.js
@@ -1,25 +1,33 @@
-
 const fs = require('fs');
 const path = require('path');
-const Product = require("./../src/database/models/product");
+const {Product, Brand} = require("./../src/database/models/index");
 
 const productsFilePath = path.join(__dirname, '../src/database/db_productos.json');
 const productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 
-
-
 const Producto = {
-    getAll:  () => {
-         Product.findAll().then(data => {
-             console.log(data)
-         })
-        return productos
+    getAll:  async() => {
+         const products = await Product.findAll({include: [{model:Brand}]});
+
+         products.forEach(product => {
+            product.getBrand().then(data => {product.Brand = data});
+         });
+        return products
     },
 
-    getById: (id) => {
-        const producto = productos.find( prod => String(prod.id) === id);
+    search: async(query) => {
+        const product = await Product.findOne({ where: { name: query } })
+        return product;
+    },
+
+    getById: async(id) => {
+        const producto = await Product.findByPk(id);
         return producto;
+    },
+
+    store: async(producto) => {
+        await Product.create(producto);
     },
 
     getByNombre: (nombre) => {
