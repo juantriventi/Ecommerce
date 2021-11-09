b9b640554c9def427e920b9a074f6964cd5f683f
juantriventi <juantriventi@hotmail.com>
2021-11-09 13:34:07 -0300

    2 seeders mas y estilos de productos

--- src/database/seeders/20211101172132-Product-seeder.js
+++ src/database/seeders/20211101172132-Product-seeder.js
@@ -21,7 +21,29 @@ module.exports = {
         state: 'disponible',
         price: 1000,
         stock:100,
-        image:`1635446758971_img.jpg`,
+        image:`1631052256021_img.jpg`,
+        brandId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Nike SB',
+        description: 'zapatillas de skate',
+        state: 'disponible',
+        price: 1990,
+        stock:100,
+        image:`Force017.jpg`,
+        brandId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Nike Air max',
+        description: 'zapatillas deportivas',
+        state: 'disponible',
+        price: 1500,
+        stock:100,
+        image:`ZapatillasNikeZoom.jpg`,
         brandId: 2,
         createdAt: new Date(),
         updatedAt: new Date()
